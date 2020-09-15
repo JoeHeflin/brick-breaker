6d57e1a0447c197682da62adcb3df4a40772ee6d
Robert Hellinga <robert.hellinga@duke.edu>
2020-09-14 21:19:17 -0400

    Finished basic implementation of Brick [UNTESTED],
    updated Ball to have its boarders as fields

---
+++ src/breakout/Ball.java
@@ -0,0 +1,78 @@
+package breakout;
+
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+import javafx.scene.shape.Circle;
+
+public class Ball {
+
+    int radius;
+    float xPos;
+    float yPos;
+
+    float up;
+    float down;
+    float left;
+    float right;
+    float speed;
+    float xVel;
+    float yVel;
+    int damage;
+
+
+    public Ball(float startingX, float startingY, float spd, int r){
+        this.radius = r;
+        this.xPos = startingX;
+        this.yPos = startingY;
+        this.up = yPos + radius;
+        this.down = yPos - radius;
+        this.left = xPos - radius;
+        this.right = xPos + radius;
+        this.xVel = 0;
+        this.yVel = 0;
+        this.speed = spd;
+        this.damage = 1;
+    }
+
+    //Makes the ball start moving // stop moving
+    void startStop(boolean go, float launchAngle){
+        if(go){
+            this.xVel = (speed * Math.cos(Math.toRadians(launchAngle));
+            this.yVel = (speed * Math.sin(Math.toRadians((launchAngle)));
+        }
+        else{
+            this.xVel = 0;
+            this.yVel = 0;
+        }
+    }
+
+    void moveAndDetectStage(Stage stage){
+
+        if(left < 0){
+            bounce(true);
+            xPos = 0;
+        }
+        if(right > stage.sizeX){
+            bounce(true);
+            xPos = stage.sizeX;
+        }
+        if(up < 0){
+            bounce(false);
+            yPos = 0;
+        }
+
+        xPos += xVel;
+        yPos += yVel;
+    }
+
+    void bounce(Boolean xOtherwiseY){
+        if(xOtherwiseY){
+            this.xVel = -this.xVel;
+        }
+        else{
+            this.yVel = -this.yVel;
+        }
+    }
+
+
+}
