0aba4fc29cffbeb7616e483bbb4bff5fa5878c65
Robert Hellinga <robert.hellinga@duke.edu>
2020-09-28 21:21:19 -0400

    Working PowerUp Implementation

--- src/breakout/DoubleBrick.java
+++ src/breakout/DoubleBrick.java
@@ -11,8 +11,8 @@ public class DoubleBrick extends Brick{
     }
 
     @Override
-    void takeDamage(Ball ball, MenuBar menuBar, LevelBuilder layout){
-        super.takeDamage(ball, menuBar, layout);
+    void takeDamage(Ball ball, MenuBar menuBar, LevelBuilder layout, PowerUpHolder powerUps){
+        super.takeDamage(ball, menuBar, layout, powerUps);
         if(health == 1){
             color = Color.RED;
             setColor();
---
+++ src/breakout/PowerUp.java
@@ -0,0 +1,40 @@
+package breakout;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+public abstract class PowerUp extends Circle {
+
+    private boolean active;
+    private double spd;
+
+    PowerUp(double x, double y){
+        super(x, y, Game.POWERUP_RADIUS);
+        active = true;
+        spd = 100;
+    }
+
+    public boolean getActive(){
+        return active;
+    }
+
+    public void usePower(Ball ball, Paddle paddle){
+        setInactive();
+    }
+
+    public void setInactive(){
+        active = false;
+        setColor(Game.BACKGROUND_COLOR);
+    }
+
+    public void setColor(Color color){
+        this.setFill(color);
+    }
+
+    void updatePosition(double elapsedTime) {
+        setCenterY(getCenterY() + this.spd * elapsedTime);
+    }
+
+
+
+
+}
